905717fb5c1f926df1ff16d1d3f4841d70c163b2
y2k <itwisterlx@gmail.com>
2020-01-26 19:40:13 +0300

    clear project

--- web/splitter.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: "web.fsproj",
-    outDir: "bin",
-    // babel: {
-    //     presets: [["@babel/preset-env"/* , { modules: "commonjs" } */]],
-    //     sourceMaps: false,
-    // }
-    // fable: { define: ["FABLE"] }
-}
